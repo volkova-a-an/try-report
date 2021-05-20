1c5f3fdb8de67b8cce453a37cb35da9fd5a34951
Anastasia Volkova <anastasia.volkova@t-systems.com>
2021-05-20 09:53:36 +0300

    add screenshot for extent report

--- extent-reports/src/test/java/com/extentreports/tests/ExtentReportTestNG.java
+++ extent-reports/src/test/java/com/extentreports/tests/ExtentReportTestNG.java
@@ -2,22 +2,35 @@ package com.extentreports.tests;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.service.ExtentTestManager;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Base64;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.reportsFolder;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-// https://www.extentreports.com/docs/versions/5/java/spark-reporter.html
-// https://www.extentreports.com/docs/versions/4/java/testng.html
-// https://www.extentreports.com/docs/versions/4/java/extentservice.html
+/**
+ * Used docs
+ * https://www.extentreports.com/docs/versions/5/java/spark-reporter.html
+ * https://www.extentreports.com/docs/versions/4/java/testng.html
+ * https://www.extentreports.com/docs/versions/4/java/extentservice.html
+ */
 
 @Listeners(ExtentITestListenerClassAdapter.class)
 public class ExtentReportTestNG {
@@ -28,16 +41,18 @@ public class ExtentReportTestNG {
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://yam.telekom.de/startpage";
         Configuration.headless = true;
+        Configuration.reportsFolder = "build/test-results/selenide-screenshot";
+        Configuration.savePageSource = false;
         open(baseUrl);
     }
 
     @Test
     public void firstTest() {
 
-// create first test
+        // create first test
         ExtentTest firstTest = extent.createTest("Open Feedback box")
 
-                // assign labels
+        // assign labels
                 .assignAuthor("Nastja")
                 .assignCategory("Smoke")
                 .assignDevice("Chrome90"); // create a test node in the report
@@ -76,13 +91,51 @@ public class ExtentReportTestNG {
         $("span.open").click();
         doomedToFail.info("Clicked Open Feedback Dialogue");
         $("div.feedback-box").shouldHave(Condition.text("Geben Sie uns Feedback!"));
-
-
-
     }
+
+    @AfterMethod
+    public void afterMethod(ITestResult result) throws IOException {
+        switch (result.getStatus()) {
+            case ITestResult.FAILURE:
+
+                File inputFile = findTheMostRecentScreenshot(reportsFolder);
+                String encodedString = encodeFileToBase64(inputFile);
+
+                ExtentTestManager.getTest(result).fail("Test failed, event afterMethod. Here is a screenshot: ",
+                        MediaEntityBuilder.createScreenCaptureFromBase64String(encodedString).build());
+                break;
+            case ITestResult.SKIP:
+                ExtentTestManager.getTest(result).skip("ITestResult.SKIP, event afterMethod");
+                break;
+            default:
+                break;
+        }
+    }
+
 
     @AfterSuite
     public void tearDown() {
         extent.flush();
+    }
+
+
+    public static File findTheMostRecentScreenshot(String sdir) {
+        File dir = new File(sdir);
+        if (dir.isDirectory()) {
+            File[] dirFiles = dir.listFiles((FileFilter) FileFilterUtils.fileFileFilter());
+            if (dirFiles != null && dirFiles.length > 0) {
+                Arrays.sort(dirFiles, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+                return dirFiles[0];
+            }
+        }
+
+        return null;
+    }
+
+    public String encodeFileToBase64(File inputFile) throws IOException {
+        byte[] fileContent = FileUtils.readFileToByteArray(inputFile);
+        return Base64
+                .getEncoder()
+                .encodeToString(fileContent);
     }
 }
